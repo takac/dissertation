497ae1360f2f3294f88e8d83eb20640d522d16db
Tom Cammann <tom.cammmann@vmci.net>
2012-04-03 19:41:30 +0100

    added more about the sim

--- watch_compile.sh
+++ watch_compile.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-_watch_compile() 
-{
-	local cur prev opts
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts="--help --verbose --version"
-
-	if [[ ${cur} == -* ]] ; then
-		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-		return 0
+# Add this to bashrc or completion dir..
+#complete -f -X '!*.@(tex)' watch_compile.sh
+if [ ${#} -gt 2 ]; then
+	echo "Usage: max 2 args expected"
+	exit
+fi
+#TODO 
+if [ ${#} == 2 ]; then
+	echo "arg echo $1"
+	if [ ${1} ]; then
+		echo "BAH"
 	fi
-	if [[ ${cur} == " " ]]; then 
-		# could use  ls -1 | while read filename; do echo "$filename"; done
-		 local name=$(ls -1 *.tex)
-		 COMPREPLY=( $(compgen -W "${names}" -- ${cur}) )
-		 return 0
-	 fi
-}
-complete -F  watch_compile watch_compile
+fi
 
 if [ -z $1 ]; then
 	1="*.tex"
@@ -35,20 +28,20 @@ else
 	fi
 fi
 
-echo "Watching $1 files"
+echo "Watching $1"
 
 while true; do
-	FILE=`inotifywait $1` 
-	LEN=`echo $FILE | wc -w`
-	FILE=`echo $FILE | cut -d " " -f$(($LEN - 1))`
+	FILE=$( inotifywait $1 ) 
+	LEN=$( echo $FILE | wc -w )
+	FILE=$( echo $FILE | cut -d " " -f$(($LEN - 1)) )
 
 	echo "$FILE modified, recompiling div"
 	sleep 1
 	#latex -interaction=nonstopmode *.tex > /dev/null
 	pdflatex -interaction=nonstopmode $FILE > /dev/null
 	#considered using y=${x%.*}
-	bibtex -interaction=nonstopmode `basename $FILE .tex` > /dev/null
-	cp intro.pdf /var/www/
+	bibtex $( basename $FILE .tex ) > /dev/null
+	cp $( basename $FILE .tex ).pdf /var/www/
 	echo "compiled"
 done
 
